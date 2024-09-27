44a25e71e543534b65487a6dbf1fc3d699248008
Aamin88 <forkahamin@yahoo.co.uk>
2024-09-27 23:50:11 +0100

    add:

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import axios from "axios";
 import "./App.css";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [startTime, setStartTime] = useState(Date.now());
   const [scrollPosition, setScrollPosition] = useState(0);
 
   // get the cookies
+  const updateServer = async () => {
+    console.log(startTime);
+    try {
+      const res = await axios.post("http://localhost:3000/update-tracking", {
+        timeSpent: Date.now() - startTime,
+        scrollPosition: scrollPosition,
+      });
+
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // Function to set a cookie on the server
   const setCookie = async () => {
     try {
-      await axios.get("https://cookies-uj9t.onrender.com/set-cookie");
+      await axios.get("http://localhost:3000/set-cookie", {
+        withCredentials: true,
+      });
       console.log("Cookie set successfully");
     } catch (error) {
       console.error("Error setting cookie:", error);
@@ -27,24 +42,10 @@ function App() {
     window.addEventListener("scroll", handleScroll);
 
     setCookie();
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    updateServer();
   }, []);
 
   useEffect(() => {
-    const updateServer = async () => {
-      try {
-        await axios.post("https://cookies-uj9t.onrender.com/update-tracking", {
-          timeSpent: Date.now - startTime,
-          scrollPosition: scrollPosition,
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     const intervalId = setInterval(updateServer, 1000);
 
     return () => {
@@ -55,6 +56,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        <button onClick={setCookie}>set cookies</button>
         <p>Time spent on this site: {startTime}</p>
         <p>Scroll position: {scrollPosition}</p>
       </div>
